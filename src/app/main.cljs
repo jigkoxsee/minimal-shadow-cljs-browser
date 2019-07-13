e28bad3ce30d95ae2f4c0858711a975743bb2c75
jigkoxsee <ked.naja@gmail.com>
2019-07-13 19:29:30 +0700

    add simple re-frame dominos

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -19,15 +19,71 @@
     {:time (js/Date.)         ;; What it returns becomes the new application state
      :time-color "#f88"}))    ;; so the application state will initially be a map with two keys
 
+
+(rf/reg-event-db                ;; usage:  (dispatch [:time-color-change 34562])
+ :time-color-change            ;; dispatched when the user enters a new colour into the UI text field
+ (fn [db [_ new-color-value]]  ;; -db event handlers given 2 parameters:  current application state and event (a vector)
+   (assoc db :time-color new-color-value)))   ;; compute and return the new application state
+
+
+(rf/reg-event-db                 ;; usage:  (dispatch [:timer a-js-Date])
+ :timer                         ;; every second an event of this kind will be dispatched
+ (fn [db [_ new-time]]          ;; note how the 2nd parameter is destructured to obtain the data value
+   (assoc db :time new-time)))  ;; compute and return the new application state
+
+
+
+(rf/reg-sub
+ :time
+ (fn [db _]     ;; db is current app state. 2nd unused param is query vector
+   (:time db))) ;; return a query computation over the application state
+
+(rf/reg-sub
+ :time-color
+ (fn [db _]
+   (:time-color db)))
+
+
+(defn dispatch-timer-event
+  []
+  (let [now (js/Date.)]
+    (rf/dispatch [:timer now])))  ;; <-- dispatch used
+
+(defonce do-timer (js/setInterval dispatch-timer-event 1000))
+
 ;;--------------
+
+(defn clock
+  []
+  [:div.example-clock
+   {:style {:color @(rf/subscribe [:time-color])}}
+   (-> @(rf/subscribe [:time])
+       .toTimeString
+       (string/split " ")
+       first)])
+
+(defn color-input
+  []
+  [:div.color-input
+   "Time color: "
+   [:input {:type "text"
+            :value @(rf/subscribe [:time-color])
+            :on-change #(rf/dispatch [:time-color-change (-> % .-target .-value)])}]])  ;; <---
+
+(defn ui
+  []
+  [:div
+   [clock]
+   [color-input]])
 
 
 
 (defn main-panel []
   [:div
-    [:h2 "hello"]
-    [:div
-     [:p "world"]]])
+   [:h2 "hello world"]
+   [:div
+    [ui]]])
+   
 
 
 (defn init []
